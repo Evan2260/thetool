5b241a446e599eed03dffd686563fbb0ea963aa7
Evan <evfernberg@gmail.com>
2022-08-13 14:05:32 -0400

    Imported button styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from './Button.js'
+import './button.css'
 
 function App() {
   return (
@@ -10,9 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <div>
-          <Button />
-        </div>
+          <div>
+            <Button />
+          </div>
         <a
           className="App-link"
           href="https://reactjs.org"
