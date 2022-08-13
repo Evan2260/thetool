2bdd5657ee2160ff7f0faff662a432dc8ecf1c7e
Evan <evfernberg@gmail.com>
2022-08-13 13:06:01 -0400

    Created and imported Button Component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './Button.js'
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <Button />
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Button.js
@@ -0,0 +1,7 @@
+const Button = (props) =>{
+  return(
+    <h1>This is the Button component.</h1>
+  )
+}
+
+export default Button
