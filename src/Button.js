d29c77802adc62a95bca867c69a2fc14fd741bed
Evan <evfernberg@gmail.com>
2022-08-13 14:05:44 -0400

    Imported button styling.

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,9 @@
+import './button.css'
+
 const Button = (props) =>{
   return(
-    <h1>This is the Button component.</h1>
+    <h1 className="button">This is the Button component.</h1>
   )
-}
+};
 
-export default Button
+export default Button;
